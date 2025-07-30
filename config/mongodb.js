63b02231891d653384ec5f87f1e52ad7fe552677
Tahsin566 <nazmulhassantahsin566@gmail.com>
2025-07-30 19:08:36 +0600

    update

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function mongoconnect(){
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI_PROD)
         console.log("Connected to mongodb")
     } catch (error) {
         console.log("Cannot connect to mongodb")
